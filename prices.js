670f8b47223bbfb88737b8cd0f394663b4b5018c
VeztooX123 <shekharxiii@gmail.com>
2026-04-06 18:10:02 +0530

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,22 +1,22 @@
-{
-  "lastUpdated": "06 April 2025",
+const VEZTOOX_PRICES = {
+  lastUpdated: "06 April 2025",
 
-  "tamatar": "₹30 /kg",
-  "aloo": "₹20 /kg",
-  "pyaz": "₹35 /kg",
-  "gajar": "₹25 /kg",
-  "matar": "₹50 /kg",
-  "kheera": "₹20 /kg",
-  "baingan": "₹30 /kg",
-  "hariMirch": "₹60 /100g",
+  tamatar: "₹30 /kg",
+  aloo: "₹20 /kg",
+  pyaz: "₹35 /kg",
+  gajar: "₹25 /kg",
+  matar: "₹50 /kg",
+  kheera: "₹20 /kg",
+  baingan: "₹30 /kg",
+  hariMirch: "₹60 /100g",
 
-  "kela": "₹40 /dozen",
-  "seb": "₹120 /kg",
-  "nimbu": "₹8 /piece",
-  "angur": "₹80 /kg",
+  kela: "₹40 /dozen",
+  seb: "₹120 /kg",
+  nimbu: "₹8 /piece",
+  angur: "₹80 /kg",
 
-  "dhaniya": "₹8 /guccha",
-  "palak": "₹20 /bundle",
-  "methi": "₹15 /bundle",
-  "pudina": "₹8 /guccha"
-}
+  dhaniya: "₹8 /guccha",
+  palak: "₹20 /bundle",
+  methi: "₹15 /bundle",
+  pudina: "₹8 /guccha"
+};
