2192040b1832186f1acd4109ebde39801dabc036
VeztooX123 <shekharxiii@gmail.com>
2026-04-06 18:21:15 +0530

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,7 +1,7 @@
 const VEZTOOX_PRICES = {
   lastUpdated: "06 April 2025",
 
-  tamatar: "₹30 /kg",
+  tamatar: "₹40 /kg",
   aloo: "₹20 /kg",
   pyaz: "₹35 /kg",
   gajar: "₹25 /kg",
@@ -10,7 +10,7 @@ const VEZTOOX_PRICES = {
   baingan: "₹30 /kg",
   hariMirch: "₹60 /100g",
 
-  kela: "₹40 /dozen",
+  kela: "₹30 /dozen",
   seb: "₹120 /kg",
   nimbu: "₹8 /piece",
   angur: "₹80 /kg",
